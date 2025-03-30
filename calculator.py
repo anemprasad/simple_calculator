ce81252bf4dd3fd7216192bd0789573833a8739a
anemprasad <40623118+anemprasad@users.noreply.github.com>
2025-03-30 11:00:22 +0000

    Initial Commit

---
+++ calculator.py
@@ -0,0 +1,22 @@
+# simple_calculator/calculator.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
+
+if __name__ == "__main__":
+    print("Calculator Operations")
+    print("Addition:", add(5, 3))
+    print("Subtraction:", subtract(5, 3))
+    print("Multiplication:", multiply(5, 3))
+    print("Division:", divide(5, 3))
